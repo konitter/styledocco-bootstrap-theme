13050648ee2e0364603d17591d67483e2d2d1841
konitter <web.kony@gmail.com>
2013-09-11 13:22:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bootstrap theme for StyleDocco
+
+This is a [Bootstrap](http://getbootstrap.com/) theme for [StyleDocco](http://jacobrask.github.com/styledocco/). StyleDocco generates documentation and style guide documents from your stylesheets.
+
+!(https://github.com/konitter/styledocco-bootstrap-theme/raw/master/img/screen.png)
+
+## How to apply this theme
+
+1. Install StyleDocco. Type ``npm install styledocco`` or ``npm install -g styledocco`` for global CLI.
+2. Download [styledocco-bootstrap-theme](https://github.com/konitter/styledocco-bootstrap-theme/archive/master.zip) and copy the "share" folder into your StyleDocco installed directory. The original "share" folder is recommended that you back up.
+3. Run ``styledocco`` command, like below.
+
+```
+styledocco [options] [STYLESHEET(S)]
+```
---
+++ share/docs.ui.js
@@ -0,0 +1,52 @@
+// StyleDocco documentation user interface elements
+// ==================================================================
+// Dropdowns and search.
+
+(function() {
+
+'use strict';
+/*global searchIndex:false*/
+
+// Helper functions. Using `Array.prototype` to make them work on NodeLists.
+var inArray = function(arr, item) {
+  return Array.prototype.indexOf.call(arr, item) !== -1;
+};
+var filter = function(arr, iterator) {
+  return Array.prototype.filter.call(arr, iterator);
+};
+var forEach = function(arr, iterator) {
+  return Array.prototype.forEach.call(arr, iterator);
+};
+
+var bodyEl = document.getElementsByTagName('body')[0];
+
+(function() {
+  var tocList = bodyEl.getElementsByClassName('bs-sidenav')[0];
+  if (!tocList) return;
+
+  filter(bodyEl.getElementsByTagName('*'), function(el) {
+    return inArray(['h1', 'h2', 'h3'], el.tagName.toLowerCase());
+  }).map(function(h) {
+    var el = document.createElement('li');
+    var a = document.createElement('a');
+    var lv = h.tagName.toLowerCase()[1];
+    a.classList.add('lv' + lv);
+    el.appendChild(a);
+    a.href = '#' + h.id;
+    a.innerHTML = h.innerHTML;
+    tocList.appendChild(el);
+  });
+
+})();
+
+(function() {
+  var previewList = bodyEl.getElementsByClassName('preview-code');
+  if (!previewList) return;
+
+  forEach(previewList, function(el) {
+    el.classList.add('highlight');
+  });
+
+})();
+
+})();
